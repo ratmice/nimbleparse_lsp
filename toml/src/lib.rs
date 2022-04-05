5369afc87e35a342c26636502110d8a15f5d9c85
matt rice <ratmice@gmail.com>
2022-04-05 14:41:08 -0700

    Add yacckind to configuration

--- toml/src/lib.rs
+++ toml/src/lib.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
+use cfgrammar::yacc::{YaccKind, YaccOriginalActionKind};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Workspace {
@@ -12,6 +13,14 @@ pub struct Parser {
     pub l_file: toml::Spanned<PathBuf>,
     pub y_file: toml::Spanned<PathBuf>,
     pub extension: toml::Spanned<String>,
+    // Spanned doesn't have a public constructor
+    // So it doesn't play nice with
+    #[serde(default = "default_yacc_kind")]
+    pub yacc_kind: YaccKind,
+}
+
+fn default_yacc_kind() -> YaccKind {
+    YaccKind::Original(YaccOriginalActionKind::GenericParseTree)
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
